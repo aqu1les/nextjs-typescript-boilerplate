4565928995a894af2edc85526d740db0e05d2e95
Felipe Barros <felipebarros.dev@gmail.com>
2020-07-18 20:26:21 -0300

    added project configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by&nbsp;
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
